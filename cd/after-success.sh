1488744a5d4fa1916f28c03b1016dd0695f494d9
Chris Kolek <christopher.w@kolek.me>
2018-02-07 11:20:43 -0500

    Only publish to maven on master and stable branches

--- cd/after-success.sh
+++ cd/after-success.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ] && ([ "$TRAVIS_BRANCH" = "master" ] || ([ "$TRAVIS_BRANCH" = "stable" ] && [ "$TRAVIS_TAG" != "" ])); then
     ./gradlew uploadArchives
 fi
